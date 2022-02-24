c82b11bbcc0c89a4e4e279f699164aa769610b45
tangxifan <tangxifan@gmail.com>
2022-02-24 11:08:28 -0800

    [RTL] Remove parameters in adder benchmarks as they canot be parsed by pyverilog

--- simple_gates/adder/adder_4/adder_4.v
+++ simple_gates/adder/adder_4/adder_4.v
@@ -1,11 +1,10 @@
 // Creating a scaleable adder
 
 module adder_4(cout, sum, a, b, cin);
-parameter size = 4;  /* declare a parameter. default required */
 output cout;
-output [size-1:0] sum; 	 // sum uses the size parameter
+output [3:0] sum; 	 // sum uses the size parameter
 input cin;
-input [size-1:0] a, b;  // 'a' and 'b' use the size parameter
+input [3:0] a, b;  // 'a' and 'b' use the size parameter
 
 assign {cout, sum} = a + b + cin;
 
--- simple_gates/adder/adder_8/adder_8.v
+++ simple_gates/adder/adder_8/adder_8.v
@@ -1,11 +1,10 @@
 // Creating a scaleable adder
 
 module adder_8(cout, sum, a, b, cin);
-parameter size = 8;  /* declare a parameter. default required */
 output cout;
-output [size-1:0] sum; 	 // sum uses the size parameter
+output [7:0] sum; 	 // sum uses the size parameter
 input cin;
-input [size-1:0] a, b;  // 'a' and 'b' use the size parameter
+input [7:0] a, b;  // 'a' and 'b' use the size parameter
 
 assign {cout, sum} = a + b + cin;
 
