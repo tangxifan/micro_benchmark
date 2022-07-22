970869bb31534b352eca1846abc70ec2706b1d96
usmanali-rs <usman.ali@rapidsilicon.com>
2022-07-22 11:25:43 +0000

    adding benchmark to test negedge flip-flops

---
+++ simple_registers/ff/dffnr/dffnr.v
@@ -0,0 +1,14 @@
+///////////////////////////////////////////////
+//  Functionality: Active high async resetable,
+//		negedge triggered D flip-flop
+///////////////////////////////////////////////
+
+module dffnr(clk_i, clr_i, d_i,q_o);
+
+	input clk_i, clr_i,d_i;
+	output reg q_o;
+	always @ (negedge clk_i or posedge clr_i) begin
+		q_o <= (clr_i)? 1'b0: d_i;
+	end
+
+endmodule
